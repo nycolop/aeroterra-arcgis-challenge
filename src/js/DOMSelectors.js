671e4c3133f61aa303cd55aa27d0f0f3c6bb334b
necogamy <madrazonecogamy@gmail.com>
2022-03-17 09:50:52 -0300

    Made form validations

---
+++ src/js/DOMSelectors.js
@@ -0,0 +1,12 @@
+const DOMForm = {
+    form: document.querySelector('form'),
+    name: document.getElementById('name'),
+    address: document.getElementById('address'),
+    phone: document.getElementById('phone'),
+    category: document.getElementById('category'),
+    coords: {
+        coordX: document.getElementById('coord-x'),
+        coordY: document.getElementById('coord-y')
+    },
+    button: document.getElementById('submit')
+}
---
+++ src/js/formValidation.js
@@ -0,0 +1,74 @@
+window.addEventListener('load', () => {
+    DOMForm.form.addEventListener('submit', e => {
+        e.preventDefault();
+        validateFields();
+    });
+
+    const validateFields = () => {
+        const values = {
+            nameValue: DOMForm.name.value,
+            addressValue: DOMForm.address.value,
+            phoneValue: DOMForm.phone.value,
+            categoryValue: DOMForm.category.value,
+            coordXValue: DOMForm.coords.coordX.value,
+            coordYValue: DOMForm.coords.coordY.value
+        }
+
+        // Name Input
+        if (!values.nameValue) {
+            validateError(DOMForm.name, 'Campo requerido');
+        } else {
+            validateSuccess(DOMForm.name);
+        }
+
+        // Address Input
+        if (!values.addressValue) {
+            validateError(DOMForm.address, 'Campo requerido');
+        } else {
+            validateSuccess(DOMForm.address);
+        }
+
+        // Phone Input
+        if (!values.phoneValue) {
+            validateError(DOMForm.phone, 'Campo requerido');
+        } else {
+            validateSuccess(DOMForm.phone);
+        }
+
+        // Category Input
+        if (values.categoryValue === "default") {
+            validateError(DOMForm.category, 'Campo requerido');
+        } else {
+            validateSuccess(DOMForm.category);
+        }
+
+        // Coordenadas Inputs
+        if (!values.coordXValue) {
+            validateError(DOMForm.coords.coordX, 'Campo requerido');
+        } else if (values.coordXValue > 180 || values.coordXValue < -180) {
+            validateError(DOMForm.coords.coordX, 'El valor debe ser entre -180 y 180');
+        } else {
+            validateSuccess(DOMForm.coords.coordX);
+        }
+
+        if (!values.coordYValue) {
+            validateError(DOMForm.coords.coordY, 'Campo requerido');
+        } else if (values.coordYValue > 90 || values.coordYValue < -90) {
+            validateError(DOMForm.coords.coordY, 'El valor debe ser entre -90 y 90');
+        } else {
+            validateSuccess(DOMForm.coords.coordY);
+        }
+    }
+
+    const validateSuccess = input => {
+        const formControl = input.parentElement;
+        formControl.className = 'validate-success';
+    }
+
+    const validateError = (input, message) => {
+        const formControl = input.parentElement;
+        const alert = formControl.querySelector('small');
+        alert.innerText = message;
+        formControl.className = 'validate-error';
+    }
+});
